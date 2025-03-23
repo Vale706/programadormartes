08a723f7ec15a4f78f0f9ff20a0b97e83694a095
Andrea Segura <andree.se.07@gmail.com>
2025-03-22 21:23:50 -0300

    MODULO 5 UNIDAD 2

--- SITIO ATENEO/models/novedadesModel.js
+++ SITIO ATENEO/models/novedadesModel.js
@@ -12,5 +12,36 @@ async function deleteNovedadById(id){
     var query = 'delete from novedades where id = ?';
     var rows = await pool.query(query,[id]);
     return rows;
-}    
-module.exports={getNovedades, deleteNovedadById}
+} 
+
+//Funcion para Agregar Novedad
+async function insertNovedad(obj){
+    try{
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+//MODIFICAR Función
+async function getNovedadById(id){
+    var query = "select * from novedades where id=?";
+    var rows = await pool.query(query,[id]);
+    return rows[0];
+}
+
+//Modificación de datos
+async function modificarNovedadById(obj, id){
+    try{
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj,id]);
+        return rows;
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+module.exports={getNovedades, deleteNovedadById, insertNovedad, getNovedadById, modificarNovedadById}
--- SITIO ATENEO/routes/admin/novedades.js
+++ SITIO ATENEO/routes/admin/novedades.js
@@ -14,12 +14,82 @@ router.get('/', async function (req, res, next) { //Aca recibo la barra
   }); 
 }),
 
-// Se agrega el controlador que captura las rutas de eliminacion y llama 
+//ELIMINAR -> Se agrega el controlador que captura las rutas de eliminacion y llama 
 // a la funcion en el Modulo. Pasa por parametro lo que reciba por url.
 router.get('/eliminar/:id', async(req, res, next) =>{
   var id = req.params.id;
   await novedadesModel.deleteNovedadById(id);
   res.redirect('/admin/novedades')
 });
+
+//AGREGAR -> Para que se vea el formulario de Agregar
+router.get('/agregar', (req,res, next) =>{
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
+});
+
+
+//AGREGAR funcion POST
+router.post('/agregar',async (req, res, next)=>{
+  
+  try{//Primero prueba que no esten vacios los campos
+    if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+      await novedadesModel.insertNovedad(req.body);
+
+      res.redirect('/admin/novedades')
+    } else{
+      res.render('admin/agregar',{
+        layout:'admin/layout',
+        error: true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch(error){
+    console.log(error)
+    res.render('admin/agregar',{
+      layout:'admin/layout',
+      error: true,
+      message:'No se cargo la novedad'
+    })
+  }
+})
+
+//Función get para Modificar
+
+router.get('/modificar/:id', async (req, res, next) =>{
+  var id = req.params.id;
+
+  var novedad =await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', { //Muestra el hbs de Modificar
+    layout:'admin/layout',
+    novedad
+  });
+});
+
+//AGREGAR funcion POST
+router.post('/modificar',async (req, res, next)=>{
+  
+  try{
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulos: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+
+    console.log(obj)//para ver si trae los datos
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch(error){
+    console.log(error)
+    res.render('admin/modificar',{
+      layout:'admin/layout',
+      error: true,
+      message:'No se modifico la novedad'
+    })
+  }
+});
+
 
 module.exports = router;
